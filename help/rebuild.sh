4292458bce557b53d9cb422149f3381d4bd9d7a3
mateusz.viste <>
2025-08-27 14:48:47 +0000

    improved build system for the HELP, includes mvcomp compression
    git-svn-id: svn://svn.svardos.org/svardos@2416 911cea91-c70f-4353-bd03-772f58fe8c9d

--- help/rebuild.sh
+++ help/rebuild.sh
@@ -2,10 +2,11 @@
 #
 # builds AMB-based help files for each available language
 # this is a Linux shell script that requires following tools to be in path:
-#  - ambpack  - http://amb.osdn.io/
-#  - utf8tocp - https://utf8tocp.sourceforge.io/
-#  - zip      - https://infozip.sourceforge.net/
 #  - advzip   - http://www.advancemame.it/
+#  - ambpack  - http://mateusz.fr/amb/
+#  - mvcomp   - http://mateusz.fr/mvcomp/
+#  - utf8tocp - http://mateusz.fr/utf8tocp/
+#  - zip      - https://infozip.sourceforge.net/
 #
 
 set -e
@@ -14,21 +15,39 @@ VER=`date +%Y%m%d`
 
 ##### amb-pack all languages #####
 
+function buildamb() {
+  folder=$1
+  cp=$2
+  title=$3
+
+  mkdir tmp
+
+  cp $folder/* tmp
+  utf8tocp -d $cp tmp/unicode.map
+  echo "$3" > tmp/title.utf8
+  utf8tocp $cp tmp/title.utf8 tmp/title
+  rm tmp/title.utf8
+  for f in tmp/*.ama ; do
+    mv $f $f.utf8
+    utf8tocp $cp $f.utf8 $f
+    mvcomp $f tmp/`basename --suffix .ama $f`.amc
+    rm $f $f.utf8
+  done
+  ambpack c tmp $folder.amb
+  rm tmp/*
+  rmdir tmp
+}
+
+
 # EN
-echo "SVARDOS HELP SYSTEM [$VER]" > help-en/title
-ambpack c help-en help-en.amb
+buildamb help-en 437 "SVARDOS HELP SYSTEM [$VER]"
 
 # DE
-echo "SVARDOS-HILFESYSTEM [$VER]" > help-de/title
-utf8tocp -d 858 help-de/unicode.map
-ambpack cc help-de help-de.amb
-rm help-de/unicode.map
+buildamb help-de 858 "SVARDOS-HILFESYSTEM [$VER]"
 
 # BR
-echo "SISTEMA DE AJUDA DO SVARDOS [$VER]" > help-br/title
-utf8tocp -d 858 help-br/unicode.map
-ambpack cc help-br help-br.amb
-rm help-br/unicode.map
+buildamb help-br 858 "SISTEMA DE AJUDA DO SVARDOS [$VER]"
+
 
 mkdir bin
 mkdir help
