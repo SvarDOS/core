b6408497477cc99a3e71d52eaf79e1780d9c85de
bernd.boeckmann <>
2025-10-09 20:53:53 +0000

    package build script for vsbhda
    git-svn-id: svn://svn.svardos.org/svardos@2468 911cea91-c70f-4353-bd03-772f58fe8c9d

---
+++ pkgtools/recipes/vsbhda/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+NAME=vsbhda
+VERSION=1.7
+CATEGORY=drivers
+DESCRIPTION="Sound Blaster emulation for HDA (and AC'97/SB Live!). (386+)"
+HWREQ=386
+
+DISTURL="https://github.com/Baron-von-Riedesel/VSBHDA/releases/download/v$VERSION/vsbhda17.zip"
+SRCURL="https://github.com/Baron-von-Riedesel/VSBHDA/archive/refs/tags/v$VERSION.zip"
+
+source ../functions.sh
+
+# commands that modify the extracted distribution files under $BUILDDIR
+# you can add, modify, delete files that will go into the .SVP package
+function adjust_builddir() {
+	rm -rf "$BUILDDIR/win31"
+	true
+}
+
+run
