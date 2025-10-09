0e96b6124149e59d472d00a23884c02b8168ffe2
mateusz.viste <>
2025-10-09 09:11:47 +0000

    core-alt packages may store documentation as PKGFUNC.TXT
    git-svn-id: svn://svn.svardos.org/svardos@2461 911cea91-c70f-4353-bd03-772f58fe8c9d

--- buildidx/buildidx.php
+++ buildidx/buildidx.php
@@ -419,7 +419,9 @@ foreach ($pkgfiles as $fname) {
       if (str_head_is($f, "doc/{$pkgdir}/")) continue;
       if (str_head_is($f, "doc/{$pkgnam}/")) continue;
       if ($f === "doc/{$pkgnam}.txt") continue;
+      if ($f === "doc/{$pkgdir}.txt") continue;
       if ($f === "doc/{$pkgnam}.amb") continue;
+      if ($f === "doc/{$pkgdir}.amb") continue;
       if ($f === 'doc/') continue;
       if (str_head_is($f, "nls/{$pkgdir}.")) continue;
       if ($f === 'nls/') continue;
