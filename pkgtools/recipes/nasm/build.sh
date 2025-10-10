01777651f7cf252aadfa02d116cbbc37791b7fc6
bernd.boeckmann <>
2025-10-10 08:47:40 +0000

    add package build script for NASM 3.00
    git-svn-id: svn://svn.svardos.org/svardos@2470 911cea91-c70f-4353-bd03-772f58fe8c9d

---
+++ pkgtools/recipes/nasm/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+NAME=nasm
+VERSION=3.00
+CATEGORY=devel
+HWREQ=386
+DESCRIPTION="The Netwide Assembler (UPX-compressed, 386+)"
+
+DISTURL="https://www.nasm.us/pub/nasm/releasebuilds/$VERSION/dos/nasm-$VERSION-dos-upx.zip"
+SRCURL="https://www.nasm.us/pub/nasm/releasebuilds/$VERSION/nasm-$VERSION.zip"
+
+source ../functions.sh
+
+# commands that modify the extracted distribution files under $BUILDDIR
+# you can add, modify, delete files that will go into the .SVP package
+function adjust_builddir() {
+	mv $BUILDDIR/nasm-$VERSION/* $BUILDDIR
+	rm -rf $BUILDDIR/nasm-$VERSION
+}
+
+run
